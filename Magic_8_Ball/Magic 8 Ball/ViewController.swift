f4e3de778730f253a288810553e1e3e4bf8e75b2
Joel Beilis <43942062+JoelBeilis@users.noreply.github.com>
2023-07-17 18:11:35 -0400

    Magic 8 completed

---
+++ Magic_8_Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Angela Yu on 14/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var ballImages: UIImageView!
+    
+    @IBAction func askButton(_ sender: Any) {
+        ballImages.image = ballArray.randomElement()
+    }
+    
+    let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5")]
+
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?){
+        if motion == .motionShake {
+            ballImages.image = ballArray.randomElement()
+        }
+    }
+
+}
+
